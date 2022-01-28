bf931245057a0c8b8a187692ad95093d29783cce
Dhruv Patel <pateldhruv9825@gmail.com>
2022-01-28 16:01:31 -0500

    Added code for ICE-3

--- Scripts/app.js
+++ Scripts/app.js
@@ -2,9 +2,26 @@
 // AKA Anonymous Self-Executing Function
 (function()
 {
+    function DisplayAboutPage()
+    {
+        console.log("About Us Page");
+    }
+
+    function DisplayProductsPage()
+    {
+        console.log("Products Page");
+    }
+
+    function DisplayServicesPage()
+    {
+        console.log("Services Page");
+    }
 
     function DisplayHomePage()
     {
+
+        console.log("Home Page");
+
         let AboutUsButton = document.getElementById("AboutUsButton");
         AboutUsButton.addEventListener("click", function()
         {
@@ -32,7 +49,7 @@
         Article.innerHTML = ArticleParagraph;
         DocumentBody.appendChild(Article);
 
-        //deletion example
+        // Deletion example
         // document.getElementById("ArticleParagraph").remove();
 
         // let NewH1 = document.createElement("h1");
@@ -41,6 +58,75 @@
         // MainContent.before(NewH1);
 
         // NewH1.textContent = "Hello, World!";
+
+
+    }
+
+    function DisplayContactPage()
+    {
+        console.log("Contact Us Page");
+
+        let sendButton = document.getElementById("sendButton");
+        let subscribeCheckBox = document.getElementById("subscribeCheckBox");
+
+        // localStorage.setItem("1", "Dhruv");
+        // console.log(localStorage.getItem("1"));
+        // localStorage.removeItem("1");
+        // console.log(localStorage.length);
+
+        sendButton.addEventListener("click", function()
+        {
+            //event.preventDefault(); // for Debugging
+            if(subscribeCheckBox.checked)
+            {
+                let contact = new Contact(fullName.value, contactNumber.value, emailAddress.value);
+                if(contact.serialize())
+                {
+                    let key = contact.FullName.substring(0, 1) + Date.now();
+
+                    localStorage.setItem(key, contact.serialize());
+                }
+            }
+        });
+    }
+
+    function DisplayContactListPage()
+    {
+        console.log("Contact List Page");
+
+        if(localStorage.length > 0) // check if localStorage has soemthing in it
+        {
+            let contactList = document.getElementById("contactList");
+
+            let data = "";
+
+            let keys = Object.keys(localStorage);
+
+            let index = 1;
+
+            //for every key in the keys collection loop
+            for(const key of keys)
+            {
+                let contactData = localStorage.getItem(key); // retrieve contact data from localstorage
+
+                let contact = new Contact(); // creates an empty Contact object
+                contact.deserialize(contactData);
+
+                data += `<tr>
+                <th scope="row" class="text-center">${index}</th>
+                <td>${contact.FullName}</td>
+                <td>${contact.ContactNumber}</td>
+                <td>${contact.EmailAddress}</td>
+                <td></td>
+                <td></td>
+                </tr>
+                `;
+
+                index++;
+            }
+
+            contactList.innerHTML = data;
+        }
     }
 
     // named function
@@ -48,11 +134,26 @@
     {
         console.log("App Started!!");
 
-        switch(document.title)
+        switch (document.title) 
         {
-            case "Home":
-                DisplayHomePage();
-                break;
+          case "Home":
+            DisplayHomePage();
+            break;
+          case "Contact Us":
+            DisplayContactPage();
+            break;
+          case "Contact-List":
+            DisplayContactListPage();
+            break;
+          case "About Us":
+            DisplayAboutPage();
+            break;
+          case "Our Products":
+            DisplayProductsPage();
+            break;
+          case "Our Services":
+            DisplayServicesPage();
+            break;
         }
 
         
